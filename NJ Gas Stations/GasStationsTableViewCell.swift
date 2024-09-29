9efbde23913554de3b222ac004b1ecbf9d18c5dc
anjalinarang <aknarang03@gmail.com>
2024-09-29 11:15:19 -0400

    started UI for cell; associated class with cell

---
+++ NJ Gas Stations/GasStationsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GasStationsTableViewCell.swift
+//  NJ Gas Stations
+//
+//  Created by Anjali Narang  on 9/29/24.
+//
+
+import UIKit
+
+class GasStationsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
