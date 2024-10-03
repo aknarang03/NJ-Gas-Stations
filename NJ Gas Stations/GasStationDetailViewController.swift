7e4fd0f2be2e6216fa2997e048a5fcde7d616c09
anjalinarang <aknarang03@gmail.com>
2024-10-03 13:19:28 -0400

    slider works

--- NJ Gas Stations/GasStationDetailViewController.swift
+++ NJ Gas Stations/GasStationDetailViewController.swift
@@ -41,15 +41,14 @@ class GasStationDetailViewController: UIViewController {
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func priceSliderChange(_ sender: UISlider) {
+        let priceValue = sender.value
+        price.text = String (format: "$%.2f", priceValue)
     }
-    */
-
+    
+//    @IBAction func sliderValueSet(_ sender: UISlider) {
+//        let fillValue = sender.value
+//        price.text = String (format: "%.2f", fillValue)
+//    }
+    
 }
